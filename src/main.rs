691c784ae02805acba616280411b6d22ee9a5d08
Sharan Chakradhar Madduri <sharanchakradhar@outlook.com>
2024-12-19 14:34:10 +1000

    DevTrack: Files: main.rs, test-bin-rust2048.json, bin-rust2048.json. [main]  | Changes in: main.rs; test-bin-rust2048.json; bin-rust2048.json; bin-rust2048.json

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ grid()
 fn grid() {
 
    let vector_of_vector: Vec<Vec<u64>> = vec! [
-    vec![1 , 1 , 1 , 1 ],
-    vec![1 , 1 , 1 , 1 ],
-    vec![1 , 1 , 1 , 1 ],
-    vec![1 , 1 , 1 , 1 ]
+    vec![0 , 0 , 0 , 0 ],
+    vec![0 , 0 , 0 , 0 ],
+    vec![0 , 0 , 0 , 0 ],
+    vec![0 , 0 , 0 , 0 ]
    ];
 
-   println!("{} {} {} {} {}", {}, {},{},{},vector_of_vector);
+   println!("{:#?}",vector_of_vector);
 }
