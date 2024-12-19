08e6ecd69b776884b5b399dc8e85cd32e086e07a
Sharan Chakradhar Madduri <sharanchakradhar@outlook.com>
2024-12-19 18:52:48 +1000

    DevTrack: Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-fn main() {
-let mygrid = grid();
-println!("{:#?}",mygrid);
-}
-
-
-
+ // Initialize a 4x4 grid of zeros using Vec
 fn grid()->  Vec<Vec<u64>> {
 
-   let vector_of_vector: Vec<Vec<u64>> = vec! [
+   vec! [
     vec![0 , 0 , 0 , 0 ],
     vec![0 , 0 , 0 , 0 ],
     vec![0 , 0 , 0 , 0 ],
     vec![0 , 0 , 0 , 0 ]
-   ];
-
-   vector_of_vector
+   ]
 }
+
+fn print_grid (grid: &Vec<Vec<u64>>) {
+
+   for row in grid {
+      for &cell in row {
+         print!("{:2} ",cell);
+      }
+      println!(); 
+   }
+   println!();
+}
+
+
+fn main() {
+   let mut my_grid = grid();
+     
+   my_grid[0][0] = 2;
+   my_grid[1][3] = 2;
+   my_grid[3][3] = 2;
+
+   print_grid(&my_grid);
+
+   }
+   
